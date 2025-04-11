9c9e5b6d02e3b1efbb453cd37307a0949b624c29
Ryza Alicaway <ryzamaealicaway@gmail.com>
2025-04-11 19:24:53 +0800

    Separated Data

---
+++ POSBusinessDataLogic/POSProcess.cs
@@ -0,0 +1,27 @@
+﻿namespace POS.BusinessDataLogic
+{
+    public class POSProcess
+    {
+        // Login validation  
+        POSDataService.POSDataService account = new POSDataService.POSDataService(); // Add namespace here
+        static short loginAttempts = 0;  // Track login attempts  
+
+        public bool LogInValid(string userInput, string passInput)
+        {
+            return account.ValidateUserAccount(userInput, passInput);
+        } 
+
+        public bool LogInAttempts()
+        {
+            loginAttempts++;
+
+            if (loginAttempts == 3)
+            {
+                loginAttempts = 0;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ POSDataService/POSDataService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POSCommon;
+
+namespace POSDataService
+{
+    public class POSDataService
+    {
+        List<POSAccount> account = new List<POSAccount>();
+
+        public POSDataService()
+        {
+            CreateAccounts();
+        }
+        private void CreateAccounts()
+        {
+            account.Add(new POSAccount
+            {
+                Username = "admin",
+                Password = "admin123",
+            });
+        }
+        public bool ValidateUserAccount(string username, string password)
+        {
+            foreach (var account in account)
+            {
+                if (account.Username == username && account.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
